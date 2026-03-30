d24246ada8c15debeeeb0040f4967ba95823b5f5
Vinz Solano <bamharu228@gmail.com>
2026-03-30 13:08:57 +0800

    Adjusted configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async redirects() {
+    return [
+      {
+        // If someone tries to access ANY dashboard route...
+        source: '/:path(faculty|security|superadmin)/:slug*',
+        has: [
+          {
+            type: 'header',
+            key: 'cookie',
+            // ...but doesn't have the Supabase auth cookie, send them to login
+            value: '^(?!.*sb-.*-auth-token).*$', 
+          },
+        ],
+        permanent: false,
+        destination: '/login',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
